15740e8c3e036292bdff3d6e00a09e84fec4912f
hrhabib1 <sayefhabib123@gmail.com>
2022-10-20 11:30:18 +0600

    set auth

--- src/component/LogIn/LogIn.js
+++ src/component/LogIn/LogIn.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LogIn.css';
 const LogIn = () => {
     return (
-        <div>
-            <h1>This is log in page</h1>
+        <div className='form-container'>
+            <h2 className="form-title">Login</h2>
+            <form action="">
+                <div className="email">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="" required/>
+                </div>
+                <div className="password">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="" required/>
+                </div>
+                <button className='loginbtn'>Login</button>
+            </form>
+            <p  className='link'>Are you new? <Link to='/signUp'>Please sign up</Link></p>
         </div>
     );
 };
--- src/component/SignUp/SignUp.js
+++ src/component/SignUp/SignUp.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css';
 const SignUp = () => {
     return (
-        <div>
-            <h1>This is sign up page</h1>
+        <div className='form-container'>
+            <h2 className="form-title">Sign up</h2>
+            <form action="">
+                <div className="email">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="" required/>
+                </div>
+                <div className="password">
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="" required/>
+                </div>
+                <div className="password">
+                <label htmlFor="passwordConfirm">Confirm Password</label>
+                <input type="password" name="passwordConfirm" id="" required/>
+                </div>
+                <button className='loginbtn'>Sign up</button>
+            </form>
+            <p  className='link'>Already have an account? <Link to='/logIn'>Please log in</Link></p>
         </div>
     );
 };
